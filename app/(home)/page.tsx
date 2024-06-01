008f608462fe45a5e0c4886e44a8ddadb1310192
kimys <ys2life1@naver.com>
2024-06-01 12:40:52 +0900

    컴포넌트, 페이지 추가

---
+++ app/(home)/page.tsx
@@ -0,0 +1,10 @@
+import Headers from "../components/layouts/Headers";
+
+export default function Home() {
+  return (
+    <div className="text-red-500 bg-black">
+      <Headers />
+      home
+    </div>
+  );
+}
---
+++ app/components/layouts/Headers.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Headers() {
+  return <div>headers</div>;
+}
+
+export default Headers;
