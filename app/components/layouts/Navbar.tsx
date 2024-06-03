ec2583dc7d4e916d26a9b2c1d91c80a31b112c1b
kimys <ys2life1@naver.com>
2024-06-04 00:14:04 +0900

    nav바 디자인 수정

--- app/components/layouts/Navbar.tsx
+++ app/components/layouts/Navbar.tsx
@@ -5,51 +5,53 @@ import Container from "../Container";
 function Navbar() {
   return (
     <Container>
-      <div className="w-full h-10 flex  items-center gap-5 truncate p-6 text-gray-900 bg-gradient-to-r from-teal-200 to-lime-200 hover:bg-gradient-to-l hover:from-teal-200 hover:to-lime-200 focus:ring-4 focus:outline-none focus:ring-lime-200 dark:focus:ring-teal-700 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2">
-        <div className="flex justify-center items-center text-white ">
-          <Link
-            prefetch
-            href="/warranty"
-            className="cursor-pointer hover:text-blue text-white md:text-lg sm:text-sm "
-          >
-            보증업체
-          </Link>
-        </div>
+      <div className="w-full  h-14 flex items-center gap-10 truncate p-6 bg-gradient-to-r from-teal-200 to-lime-200 hover:bg-gradient-to-l hover:from-teal-200 hover:to-lime-200  dark:focus:ring-teal-700 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2">
+        <Link
+          prefetch
+          href="/warranty"
+          className="w-20 text-lg cursor-pointer hover:text-xl hover:text-white transition-all duration-300 ease-in-out"
+        >
+          보증업체
+        </Link>
         <Link
           prefetch
           href="/verify"
-          className="cursor-pointer hover:text-blue"
+          className="w-20 text-lg cursor-pointer hover:text-xl hover:text-white transition-all duration-300 ease-in-out"
         >
           먹튀검증
         </Link>
-        <Link prefetch href="/sport" className="cursor-pointer hover:text-blue">
+        <Link
+          prefetch
+          href="/sport"
+          className="w-20 text-lg cursor-pointer hover:text-xl hover:text-white transition-all duration-300 ease-in-out"
+        >
           스포츠분석
         </Link>
         <Link
           prefetch
           href="/pickster"
-          className="cursor-pointer hover:text-blue"
+          className="w-20 text-lg cursor-pointer hover:text-xl hover:text-white transition-all duration-300 ease-in-out"
         >
           분석존
         </Link>
         <Link
           prefetch
           href="/community"
-          className="cursor-pointer hover:text-blue"
+          className="w-20 text-lg cursor-pointer hover:text-xl hover:text-white transition-all duration-300 ease-in-out"
         >
           커뮤니티
         </Link>
         <Link
           prefetch
           href="/promotion"
-          className="cursor-pointer hover:text-blue"
+          className="w-20 text-lg cursor-pointer hover:text-xl hover:text-white transition-all duration-300 ease-in-out"
         >
           홍보센터
         </Link>
         <Link
           prefetch
           href="/customer"
-          className="cursor-pointer hover:text-blue"
+          className="w-20 text-lg cursor-pointer hover:text-xl hover:text-white transition-all duration-300 ease-in-out"
         >
           고객센터
         </Link>
