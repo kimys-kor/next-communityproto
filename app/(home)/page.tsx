6ddb686adf651b64327c79d278251d561c7f15f8
kimys <ys2life1@naver.com>
2024-06-07 00:47:12 +0900

    aside 넓이 조정

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -5,7 +5,7 @@ import Login from "../components/login/Login";
 export default function Home() {
   return (
     <div className="">
-      <aside className="max-w-md w-2/5 h-full flex flex-col gap-4">
+      <aside className="max-w-sm w-1/3 h-full flex flex-col gap-4">
         <Login></Login>
         <Card title={"주간베스트"}></Card>
         <Card title={"실시간베스트"}></Card>
