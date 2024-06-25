8e61c6b62e0ef15d132e0eb2194a5ea5312a2df4
kimys <ys2life1@naver.com>
2024-06-26 02:26:02 +0900

    breadcrumb 사이즈 수정

--- app/components/BreadCrumb.tsx
+++ app/components/BreadCrumb.tsx
@@ -29,12 +29,12 @@ const Breadcrumb: React.FC<BreadcrumbProps> = ({ breadcrumbData }) => {
 
   return (
     <nav className="w-full bg-white/25 rounded-2xl shadow-md flex items-center">
-      <div className="h-12 font-bold bg-indigo-400 w-1/5 rounded-l-2xl flex justify-center items-center text-white">
+      <div className="h-16 font-bold bg-indigo-400 w-1/5 rounded-l-2xl flex justify-center items-center text-white">
         <p className="tracking-wider text-[1.25rem] font-light">
           {breadcrumbData.title}
         </p>
       </div>
-      <div className="border-solid border-indigo-400 border-4 w-4/5 h-12 flex items-center rounded-r-2xl gap-10 pl-10">
+      <div className="border-solid border-indigo-400 border-4 w-4/5 h-16 flex items-center rounded-r-2xl gap-10 pl-10">
         {breadcrumbData.subMenu.map((item, index) => (
           <Link key={index} href={item.href}>
             <span
