b9098307770ba229aefdf36423ba63622e0bfc03
kimys <ys2life1@naver.com>
2024-06-30 18:24:46 +0900

    react form추가

--- app/verify/(components)/Write.tsx
+++ app/verify/(components)/Write.tsx
@@ -3,8 +3,14 @@
 import PostEditor from "@/app/components/texteditor/PostEditor";
 import React, { useRef, useState } from "react";
 import ReactQuill from "react-quill";
+import { useForm, Resolver } from "react-hook-form";
 
-function Write() {
+type FormData = {
+  title: string;
+  amount: number;
+};
+
+const Write: React.FC = () => {
   const [content, setContent] = useState("");
   const quillRef = useRef<ReactQuill>(null);
 
@@ -16,13 +22,37 @@ function Write() {
     console.log("Content saved:", content);
   };
 
-  const [title, setTitle] = useState("");
-  const [amount, setAmount] = useState();
+  const resolver: Resolver<FormData> = async (values) => {
+    return {
+      values: values.title ? values : {},
+      errors: !values.title
+        ? {
+            title: {
+              type: "required",
+              message: "This is required.",
+            },
+          }
+        : {},
+    };
+  };
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<FormData>({ resolver });
+
+  const onSubmit = handleSubmit((data) => {
+    console.log(data);
+  });
 
   return (
     <div className="p-6 flex flex-col gap-3">
       <h1 className="text-xl font-light">피해사례 작성</h1>
-      <form className="border-solid border-gray-300 border-b">
+      <form
+        onSubmit={onSubmit}
+        className="border-solid border-gray-300 border-b"
+      >
         <div className="flex flex-col border-solid border-t">
           <div className="flex justify-between items-center h-20 border-solid border-b border-gray-300">
             <div className="w-1/3 h-full flex justify-center items-center bg-[#F2F4F9]">
@@ -30,10 +60,9 @@ function Write() {
             </div>
             <div className="w-full flex justify-center items-center h-10 pl-2">
               <input
+                {...register("title")}
                 type="text"
                 id="title"
-                value={title}
-                onChange={(e) => setTitle(e.target.value)}
                 className="truncate appearance-none border border-solid w-[100%] px-7 py-3 font-light text-base text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
                 required
               ></input>
@@ -45,10 +74,9 @@ function Write() {
             </div>
             <div className="w-full flex justify-center items-center h-10 pl-2">
               <input
+                {...register("amount")}
                 type="number"
                 id="amount"
-                value={amount}
-                onChange={(e) => setAmount(e.target.value)}
                 className="truncate appearance-none border border-solid w-[100%] px-7 py-3 font-light text-base text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
                 required
               ></input>
@@ -69,6 +97,7 @@ function Write() {
           취소
         </button>
         <button
+          type="submit"
           className="bg-indigo-500 text-white px-4 py-2 w-24 h-12 hover:bg-indigo-700"
           onClick={saveContent}
         >
@@ -77,6 +106,6 @@ function Write() {
       </div>
     </div>
   );
-}
+};
 
 export default Write;
