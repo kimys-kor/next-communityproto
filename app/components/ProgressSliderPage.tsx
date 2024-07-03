895d7b8124d7eaee9e605cb4b694a0dc24d23b89
kimys <ys2life1@naver.com>
2024-07-04 02:48:55 +0900

    메인 배너 수정

--- app/components/ProgressSliderPage.tsx
+++ app/components/ProgressSliderPage.tsx
@@ -1,30 +1,30 @@
-import pc_main_top_new02 from "../../public/images/pc_main_top_new02.gif";
+import main_top from "../../public/images/main_top.png";
 import ProgressSlider from "./ProgressSlider";
 
 export default function ProgressSliderPage() {
   const items = [
     {
-      img: pc_main_top_new02,
+      img: main_top,
       desc: "main_banner",
     },
-    {
-      img: pc_main_top_new02,
-      desc: "main_banner",
-    },
-    {
-      img: pc_main_top_new02,
-      desc: "main_banner",
-    },
-    {
-      img: pc_main_top_new02,
-      desc: "main_banner",
-    },
+    // {
+    //   img: main_top,
+    //   desc: "main_banner",
+    // },
+    // {
+    //   img: main_top,
+    //   desc: "main_banner",
+    // },
+    // {
+    //   img: main_top,
+    //   desc: "main_banner",
+    // },
   ];
 
   return (
-    <main className="relative h-48 flex flex-col justify-center bg-transparent overflow-hidden">
+    <main className="relative h-48 flex flex-col justify-center bg-transparent overflow-hidden mt-3">
       <div className=" max-w-6xl mx-auto p-3">
-        <div className="flex justify-center">
+        <div className="flex justify-center bg-[#F2F7FF]">
           <ProgressSlider items={items} />
         </div>
       </div>
