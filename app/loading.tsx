4aa410838ff8dfb1e4375c5b78ff1b5fd6ef0ab2
kimys <ys2life1@naver.com>
2024-07-07 20:51:54 +0900

    loading 추가

---
+++ app/loading.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { BeatLoader } from "react-spinners";
+
+function Loading() {
+  return (
+    <div className="flex justify-center items-center fixed left-0 top-0 w-full h-full bg-white">
+      <BeatLoader color="#712ccc" />
+    </div>
+  );
+}
+
+export default Loading;
