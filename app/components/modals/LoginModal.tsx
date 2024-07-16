47e80a15c17e20b65173a9085b0612d2c5746826
kimys <ys2life1@naver.com>
2024-07-16 14:03:52 +0900

    로그인모달 아이콘경로수정

--- app/components/modals/LoginModal.tsx
+++ app/components/modals/LoginModal.tsx
@@ -5,7 +5,7 @@ import { toast } from "react-hot-toast";
 import { signIn } from "next-auth/react";
 import { FieldValues, SubmitHandler, useForm } from "react-hook-form";
 import { FcGoogle } from "react-icons/fc";
-import { AiFillGithub } from "";
+import { AiFillGithub } from "react-icons/ai";
 
 import useRegisterModal from "@/app/hooks/useRegisterModal";
 import useLoginModal from "@/app/hooks/useLoginModal";
