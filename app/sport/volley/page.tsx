aaafb4544a50e66ee4d5c1a724a5ac3886303518
kimys <ys2life1@naver.com>
2024-07-23 16:11:53 +0900

    게시판 추가

--- app/sport/volley/page.tsx
+++ app/sport/volley/page.tsx
@@ -1,7 +1,26 @@
-import React from "react";
+import Board from "@/app/components/boards/Board";
+import ThreeBanner from "@/app/components/ThreeBanner";
+import sportMain from "/public/images/sportMain.png";
+import Image from "next/image";
 
-function page() {
-  return <div>배구자료</div>;
+export default function Page() {
+  return (
+    <div className="flex flex-col max-w-[1300px] gap-6">
+      <div className="bg-guide-intro">
+        <Image
+          className=""
+          src={sportMain}
+          width={1024}
+          height={177}
+          alt={"스포츠분석"}
+        />
+      </div>
+      <ThreeBanner />
+      <div className="flex justify-center text-3xl font-semibold">
+        <span className="text-blue">배구</span>
+        &nbsp;분석
+      </div>
+      <Board />
+    </div>
+  );
 }
-
-export default page;
